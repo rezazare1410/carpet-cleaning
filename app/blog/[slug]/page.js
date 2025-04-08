bda64c648ae93b7cc27c10b044f7af56045182a1
Reza Zare <reza@gmail.com>
2025-04-08 21:01:12 +0330

    fix: make blog/[slug] fully client-only under Edge runtime

--- app/blog/[slug]/page.js
+++ app/blog/[slug]/page.js
@@ -1,7 +1,11 @@
-import BlogPostWrapper from './BlogPostWrapper';
+export const runtime = 'edge';
 
-export const runtime = 'nodejs';
+import dynamic from 'next/dynamic';
 
+// مستقیم BlogPostWrapper رو با ssr: false ایمپورت می‌کنیم
+const BlogPostWrapper = dynamic(() => import('./BlogPostWrapper'), {
+  ssr: false,
+});
 
 export default function Page() {
   return <BlogPostWrapper />;
