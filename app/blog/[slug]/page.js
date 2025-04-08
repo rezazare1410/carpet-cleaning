ad86f0521b3d6889f066f844d8eec0675ebfc4d1
Reza Zare <reza@gmail.com>
2025-04-08 23:48:08 +0330

    fix: move runtime config to top of blog/[slug]/page.js

--- app/blog/[slug]/page.js
+++ app/blog/[slug]/page.js
@@ -1,6 +1,14 @@
-'use client';
-import BlogPostContent from './BlogPostContent';
+export const runtime = 'edge';
+
+import dynamic from 'next/dynamic';
+import { Suspense } from 'react';
+
+const BlogPostContent = dynamic(() => import('./BlogPostContent'), { ssr: false });
 
 export default function Page() {
-  return <BlogPostContent />;
+  return (
+    <Suspense fallback={<div>در حال بارگذاری مقاله...</div>}>
+      <BlogPostContent />
+    </Suspense>
+  );
 }
