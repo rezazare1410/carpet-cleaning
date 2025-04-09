949c88ab6f0783e406567c8c0f5321fa2c91a560
Reza Zare <reza@gmail.com>
2025-04-09 13:45:30 +0330

    fix: move client code to BlogPostContent and set edge runtime

--- app/blog/[slug]/page.js
+++ app/blog/[slug]/page.js
@@ -1,9 +1,6 @@
 export const runtime = 'edge';
-'use client';
 
-import dynamic from 'next/dynamic';
-
-const BlogPostContent = dynamic(() => import('./BlogPostContent'), { ssr: false });
+import BlogPostContent from './BlogPostContent';
 
 export default function Page() {
   return <BlogPostContent />;
