844620b7e2103bbee824796a5533be4cd5ca65fd
Reza Zare <reza@gmail.com>
2025-09-26 20:24:01 +0330

    add some pages for sitemap

--- app/sitemap.xml/route.js
+++ app/sitemap.xml/route.js
@@ -14,7 +14,7 @@ export async function GET() {
     "root-carpet-repair", "double-knot", "Shirazeh-doozi",
     "charm-doozi", "paregi", "pardakht", "poosidegi",
     "bidkhordegi", "sabidegi", "sarnakh", "stain-removal",
-    "darkeshi", "final-inspection"
+    "darkeshi", "final-inspection" ,"carpet-cleaning-east-tehran" , "carpet-cleaning-west-tehran" , "carpet-cleaning-north-tehran" , "carpet-cleaning-south-tehran" , "carpet-cleaning-center-tehran"
   ];
 
   const staticUrls = staticPages.map((page) => {
