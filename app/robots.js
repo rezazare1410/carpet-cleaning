441d1ef2b8d79c6451e8631be0058ad03c0d5bdb
Reza Zare <reza@gmail.com>
2026-08-01 20:54:33 +0330

    Add robots.txt for search engines

---
+++ app/robots.js
@@ -0,0 +1,10 @@
+export default function robots() {
+  return {
+    rules: {
+      userAgent: "*",
+      allow: "/",
+    },
+    sitemap: "https://zarecarpet.com/sitemap.xml",
+    host: "https://zarecarpet.com",
+  };
+}
